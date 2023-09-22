3d733431a69539f7cc7c55c6b4ed1184c9c0c23c
Couto01 <53975114+Couto01@users.noreply.github.com>
2023-09-22 07:19:34 -0300

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,33 @@
+import CardLivro from "@/components/CardLivro";
+import Titulo from "@/components/Titulo";
+
+async function carregarDados(){
+  const url = "https://openlibrary.org/subjects/fantasy.json"
+  const response = await fetch(url)
+  const json = await response.json()
+  return json.works
+}
+
+export default async function Home() {
+  
+  const livros = await carregarDados()
+
+  return (
+    <>
+      <nav className="flex items-end gap-4 bg-green-700 p-4">
+        <h1 className="text-3xl font-bold">Booker</h1>
+        <a href="/favoritos">favoritos</a>
+      </nav>
+
+      <Titulo>em alta</Titulo>
+
+      <section className="flex flex-wrap gap-2">
+        {livros.map( livro => <CardLivro livro={livro} /> )}
+      </section>
+
+      <Titulo>lançamentos</Titulo>
+
+    </>
+)
+
+}
---
+++ src/components/CardLivro.jsx
@@ -0,0 +1,55 @@
+"use client"
+import { useEffect, useState } from "react"
+import { HeartIcon } from '@heroicons/react/24/solid'
+import { HeartIcon as HeartIconOutline } from '@heroicons/react/24/outline'
+
+export default function CardLivro({livro}){
+    const [ favorito, setFavorito ] = useState(false) // hooks
+    const image_url = livro.thumbnail_url
+
+    useEffect(() => {
+        let favoritos = JSON.parse( localStorage.getItem("favoritos") ) || []
+        const favorito = favoritos.find(f => f.key === livro.key)
+        setFavorito(favorito)
+    }, [])
+
+    function favoritar(){
+        setFavorito(true)
+
+        let favoritos = JSON.parse( localStorage.getItem("favoritos") ) || []
+        favoritos.push(livro)
+        localStorage.setItem("favoritos", JSON.stringify(favoritos))
+    }
+    
+    function desfavoritar(){
+        setFavorito(false) 
+
+        let favoritos = JSON.parse( localStorage.getItem("favoritos") ) || []
+        const favoritosAtualizado = favoritos.filter(f => f.key !== livro.key )
+        localStorage.setItem("favoritos", JSON.stringify(favoritosAtualizado))
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-between gap-1 w-40 m-2 relative">
+            { favorito ? 
+                <HeartIcon 
+                    className="h-6 w-6 text-red-600 absolute top-1 right-2 cursor-pointer"
+                    onClick={desfavoritar}
+                 />
+                :
+                <HeartIconOutline 
+                    className="h-6 w-6 absolute top-1 right-2 cursor-pointer hover:text-red-600" 
+                    onClick={favoritar}
+                />
+            }
+
+            <img className="rounded h-56" src={image_url} alt="capa do livro" />
+            <span className="font-bold text-lg w-full line-clamp-1 text-center">
+                {livro.title}
+            </span>
+            <a href="#" className="bg-green-600 w-full rounded text-center py-1 hover:bg-green-900">
+                detalhes
+            </a>
+        </div>
+    )
+}
